c07cdfa4fb07792ae60448c13f115031d870801d
Rainscall <admin@typeboom.com>
2024-02-07 12:07:18 +0800

    modified:   src/script.js 	modified:   src/style.css

--- src/script.js
+++ src/script.js
@@ -20,6 +20,8 @@ async function writeInfo(IPInfo) {
         IPInfo = await fetch(apiEndpoint).then(r => r.json());
     }
 
+    userIP = IPInfo.clientIP;
+
     try {
         if (IPInfo.Location.country.iso_code && IPInfo.Location.registered_country.iso_code) {
             if (IPInfo.Location.country.iso_code === IPInfo.Location.registered_country.iso_code) {
@@ -136,7 +138,7 @@ async function writeInfo(IPInfo) {
         value.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M352 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9L370.7 96 201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L416 141.3l41.4 41.4c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V32c0-17.7-14.3-32-32-32H352zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>'
 
         base.addEventListener('click', () => {
-            window.open(`${apiEndpoint}/search/${document.querySelector('#ipInfoArea > div:nth-child(1) > div:nth-child(2)').innerText}`, '_blank')
+            window.open(`${apiEndpoint}/search/${userIP}`, '_blank')
         })
 
         base.appendChild(title);
@@ -169,8 +171,12 @@ async function writeInfo(IPInfo) {
                 return;
             }
             if (!isValidIP(input.value) && !isValidDomain(input.value)) {
-                createToast('Invalid IP');
-                return;
+                if ((input.value.startsWith('http://') || input.value.startsWith('https://')) && input.value.includes('.')) {
+                    input.value = input.value.split('/')[2].split(':')[0];
+                } else {
+                    createToast('Invalid IP');
+                    return;
+                }
             }
             createToast('Loading...', -1, '#FFF', '#414141', 'temp-search-loadingToast');
 
